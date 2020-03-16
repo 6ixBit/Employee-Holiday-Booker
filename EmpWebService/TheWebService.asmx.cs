6d50b5a41007b918407c14e41a50e473ce42ce2f
H <mn7754c@gre.ac.uk>
2020-03-16 14:43:44 +0000

    Started initial steps for algorithm implementation for constraint checking.

--- EmpWebService/TheWebService.asmx.cs
+++ EmpWebService/TheWebService.asmx.cs
@@ -56,27 +56,6 @@ namespace EmpWebService
             db.SaveChanges();
         }
 
-        // @desc Returns info about user if email is matched
-        public Employees get_employee_byEmail(string employee_email)
-        {
-            Employee employee = db.Employees.FirstOrDefault(e => e.email == employee_email);
-
-            // Write Db results to employee object
-            Employees my_employee = new Employees
-            {
-                Name_ = employee.name_,
-                ID_ = employee.Employee_ID,
-                Email = employee.email,
-                Password = employee.password,
-                Department = employee.department,
-                System_role = employee.system_role,
-                Employee_role = employee.employee_role,
-                Holiday_days_available = (int)employee.holiday_days_available
-            };
-
-            return my_employee;
-        }
-
         [WebMethod]
         public List<Holidays> get_outstanding_holiday_requests_for_employee(string email)
         {
@@ -112,6 +91,155 @@ namespace EmpWebService
             }
             return userHolidays;
         }
+
+        // NON WEB METHODS //
+
+        // @desc Returns info about user if email is matched
+        public Employees get_employee_byEmail(string employee_email)
+        {
+            Employee employee = db.Employees.FirstOrDefault(e => e.email == employee_email);
+
+            // Write Db results to employee object
+            Employees my_employee = new Employees
+            {
+                Name_ = employee.name_,
+                ID_ = employee.Employee_ID,
+                Email = employee.email,
+                Password = employee.password,
+                Department = employee.department,
+                System_role = employee.system_role,
+                Employee_role = employee.employee_role,
+                Holiday_days_available = (int)employee.holiday_days_available
+            };
+
+            return my_employee;
+        }
+
+        // @desc Returns number of days an employee has left.
+        public int get_holiday_days_available(string email)
+        {
+            Employee employee = db.Employees.FirstOrDefault(e => e.email == email);
+
+            return (int) employee.holiday_days_available;
+        }
+
+        //@desc Returns the days between holidays in an array of dates
+        public DateTime[] get_days_between_dates(DateTime holiday_start, DateTime holiday_end)
+        {
+            // Dyanimc arraylist to hold dates between days
+            var absentDates = new List<DateTime>();
+
+            // Add days between dates to arraylist
+            for (var dt = holiday_start; dt <= holiday_end; dt = dt.AddDays(1))
+            {
+                absentDates.Add((DateTime)dt);
+            }
+
+            // Convert arraylist with dates to array so they can be set to Bold in Calendar
+            DateTime[] actualAbsentDates = absentDates.ToArray();
+            return actualAbsentDates;
+        }
+
+        //@desc Checks whether array of dates are between 23rd Dec - 3rd Jan (Constrains do not apply during this)
+        public bool check_if_dates_are_in_constraint_period(DateTime[] days_between_holiday, DateTime constraint_start_date, DateTime constraint_end_date)
+        {
+            // Get dates between constraint dates
+            var days_between_constraints = get_days_between_dates(constraint_start_date, constraint_end_date);
+
+            // Now compare if any of the days from holiday request matches a date where the constraints applies.
+            foreach(var day in days_between_holiday)
+            {
+                foreach(var constraint_day in days_between_constraints)
+                {
+                    if (day.Month == constraint_day.Month && day.Day == constraint_day.Day)
+                    {   // IF days between holiday matches a day from constraint then return true, else false
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        //@desc Checks if employee has been at company for more than 5 years
+        public bool employed_longer_than_five_years(DateTime employee_join_date) 
+        {
+            // Get todays date and check if employee has been employed longer than 5 years.
+            var todays_date = DateTime.Today;
+
+            if (todays_date.Year - employee_join_date.Year > 5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        [WebMethod] // @desc Constraint checking Algorithm.
+        public void apply_constraint_checking(string emp_email, DateTime holiday_start, DateTime holiday_end)
+        {
+            // CONSTRAINTS TO BE APPLIED, FALSE BY DEFAULT
+            bool days_exceeded = false;
+            bool head_depHead_absent = false;
+            bool seniorStaff_absent = false;
+            bool department_absent = false;
+
+            // Get days left for employee.
+            var holiday_days_available = get_holiday_days_available(emp_email);
+
+            // Get days between holiday start and end
+            var days_between_holiday_request = get_days_between_dates(holiday_start, holiday_end);
+
+            // Check whether they're between 23rd Dec to 3rd Jan.
+            bool do_constraints_not_apply = check_if_dates_are_in_constraint_period(days_between_holiday_request, new DateTime(2020, 12, 23), new DateTime(2021, 01, 3)); 
+
+            // If holiday is between Dec 23rd and 3rd Jan then it returns true as constraints should not apply and holiday should be submitted.
+            if (do_constraints_not_apply == true)
+            { 
+                try
+                {   // Submit holiday with unmodified constraints.
+                    Console.WriteLine("No further constraints.");
+                    submit_holiday_request(emp_email, holiday_start, holiday_end, days_exceeded, head_depHead_absent, seniorStaff_absent, department_absent);
+                } catch (Exception es)
+                {
+                    Console.WriteLine(es);
+                }   
+            }
+
+            // Apply further constraints by getting info for employee.
+            var current_employee = get_employee_byEmail(emp_email);
+
+            // Verify whether or not employee qualifies for additional holiday days
+            bool add_additional_holiday = employed_longer_than_five_years(current_employee.Join_date);
+
+            var todays_date = DateTime.Today;
+
+            if (add_additional_holiday == true)
+            { // Compute number of days to be addded based on amount of years worked.
+                int total_holidays_days = holiday_days_available + (todays_date.Year - current_employee.Join_date.Year) / 5;
+
+                // Check if holiday requests exceeds the number of days the employee has available to them.
+                if(total_holidays_days - days_between_holiday_request.Length < 0)
+                {
+                    days_exceeded = true;
+                }
+            }
+
+            // GET IDs of head and depauty of department for the employees department
+
+            
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 
     // Employee class to hold objects with its associated value
